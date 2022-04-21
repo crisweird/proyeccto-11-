657f5c710277f72ed84a505ad744e61a2cb1adfc
crisweird <104140479+crisweird@users.noreply.github.com>
2022-04-21 13:21:28 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    OLED.clear()
+    OLED.writeStringNewLine("intensidad:" + Environment.ReadLightIntensity(AnalogPin.P1) + "%")
+    basic.pause(2000)
+    OLED.clear()
+    OLED.writeStringNewLine("pulsa a")
 })
+OLED.init(128, 64)
+OLED.writeStringNewLine("pulsa a")
